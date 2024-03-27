ffe0a820f4814632892b3f889df46bce0309c517
dong <17712102522@163.com>
2024-03-27 18:03:02 +0800

    初有成效

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { MongooseModule } from '@nestjs/mongoose';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest')],
+  // 同时连接多个数据库
+  // imports: [
+  //   MongooseModule.forRoot('mongodb://localhost/test', {
+  //     connectionName: 'cats',
+  //   }),
+  //   MongooseModule.forRoot('mongodb://localhost/users', {
+  //     connectionName: 'users',
+  //   }),
+  // ],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest'),
+    CatsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
